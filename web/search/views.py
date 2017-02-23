d5188f39efc31f55e3f1eeaf04892f518e8d45c9
Edward Francis Hayes <edwardhayes@wireless-s1-no-150-24-7.uchicago.edu>
2017-02-22 19:53:00 -0600

    commit all

---
+++ web/search/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django import forms
+
+from .forms import SubmitNeighborhood, SubmitAlteredParameters
+
+
+def start(request):
+    form = SubmitNeighborhood()
+    # args = {}
+    # args['neighborhood'] = form.cleaned_data['neighborhood']
+    context = {'form': form}
+
+    return render(request, 'index.html', context)
+
+# def alter(request, neighborhood):
+    # form = SubmitAlteredParameters()
+    # context['form'] = form
+    # return
